fcf1344455ef8684050e3290d570bb70a14a0f07
Hellwood1 <samburik123@gmail.com>
2025-08-27 22:18:06 +0200

    Upd.
    - Видалив зайві файли,
    - Змінив шлях

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/handy-service-project/',
+    base: '/',
     build: {
       sourcemap: true,
       rollupOptions: {
