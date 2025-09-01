c48b79860977f1a41b447b7b98e79f6e0c90b8d3
Hellwood1 <samburik123@gmail.com>
2025-09-01 13:23:22 +0200

    Upd. ''Header/hero''

--- src/js/testimonials-swiper.js
+++ src/js/testimonials-swiper.js
@@ -2,23 +2,25 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.testimonials-swiper-init', {
-  loop: true,
-  navigation: {
-    nextEl: '',
-    prevEl: '',
+  loop: false,
+  slidesPerView: 1, // старт для мобільного
+  spaceBetween: 12, // мобільний — 12px
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets',
   },
-  allowTouchMove: true,
-  autoHeight: true,
-  slidesPerView: 'auto',
-  spaceBetween: 12,
-  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 24,
+    },
+    // десктоп і більше
+    1440: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 94,
+    },
+  },
 });
-
-document.querySelectorAll('')
-  .forEach(btn => {
-    btn.addEventListener('click', () => {
-      btn.blur();
-    });
-  });
-
-  // Ще не закінчив з наралшуванням під ПК!
